6138fa6beba1d73f349325efd54b9e07ac827a92
CuberKatlets <homemany123@gmail.com>
2021-04-25 17:56:35 +0900

    0.2

--- Assets/Script/Scenamanager.cs
+++ Assets/Script/Scenamanager.cs
@@ -8,12 +8,12 @@ public class Scenamanager : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "EndLevel01")
-            SceneManager.LoadScene("SceneMinigame01");
+            SceneManager.LoadScene("Note1");
         if (collision.gameObject.name == "EndLevel02")
             SceneManager.LoadScene("Scene02");
         if (collision.gameObject.name == "EndLevel03")
-            SceneManager.LoadScene("SceneMinigame02");
+            SceneManager.LoadScene("Note2");
         if (collision.gameObject.name == "EndLevel04")
-            SceneManager.LoadScene("Scene03");
+            SceneManager.LoadScene("Scene04");
     }
 }
